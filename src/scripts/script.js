af82495c8c346378421c7cc8b03a253f9539e777
shgtkshruch <shgtk.shruch@gmail.com>
2016-02-20 14:27:08 +0900

    Initial rendering

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,1 +1,19 @@
-console.log('hello react!');
+(function () {
+  'use strict';
+
+  var rootElement = React.createElement('div', {},
+    React.createElement('h1', {},'contacts'),
+    React.createElement('ul', {},
+      React.createElement('li', {},
+        React.createElement('h2', {}, 'James Nelson'),
+        React.createElement('a', {href: 'mailto:james@jamesknelson.com'}, 'james@jamesknelson.com')
+      ),
+      React.createElement('li', {},
+        React.createElement('h2', {}, 'Joe Citizen'),
+        React.createElement('a', {href: 'mailto:joe@example.com'}, 'joe@example.com')
+      )
+    )
+  )
+
+  ReactDOM.render(rootElement, document.getElementById('react-app'));
+}());
